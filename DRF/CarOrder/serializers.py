3d0abebff062bde54a1beb0ade8beae5cd573c45
perfectrider <konsgrebenev@gmail.com>
2022-11-09 17:27:03 +0300

    Light refactoring

--- DRF/CarOrder/serializers.py
+++ DRF/CarOrder/serializers.py
@@ -1,21 +1,12 @@
-import io
-
 from rest_framework import serializers
-from rest_framework.parsers import JSONParser
-from rest_framework.renderers import JSONRenderer
-
 from .models import *
 
 
-class OrderSerializer(serializers.ModelSerializer):
+class OrderSerializer(serializers.Serializer):
     order_date = serializers.DateField()
     car_model = serializers.SlugRelatedField(slug_field='model_of_car', queryset=CarModel.objects)
     car_color = serializers.SlugRelatedField(slug_field='color', queryset=CarColor.objects)
     count = serializers.IntegerField()
-
-    class Meta:
-        model = Order
-        fields = ('order_date', 'car_model', 'car_color', 'count')
 
     def create(self, validated_data):
         return Order.objects.create(**validated_data)
--- DRF/CarOrder/views.py
+++ DRF/CarOrder/views.py
@@ -1,6 +1,3 @@
-from django.forms import model_to_dict
-from django.shortcuts import render
-from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
